982b5b4d70463ddea6d1930c3fc84d930ca03cde
krls2020 <mail@karelhemza.cz>
2024-06-12 09:36:25 +0200

    t

--- app.py
+++ app.py
@@ -61,4 +61,5 @@ def status_check():
 
 if __name__ == '__main__':
     init_db()
-    serve(app, host="0.0.0.0", port=8000)
+    app.run(host='0.0.0.0', port=8000)
+#     serve(app, host="0.0.0.0", port=8000)
