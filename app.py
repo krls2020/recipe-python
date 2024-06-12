5ca5178af2193b522b32e7c0aa6c82818e11e999
krls2020 <mail@karelhemza.cz>
2024-06-12 09:17:19 +0200

    c

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def add_entry():
     cur.close()
     conn.close()
 
-    // LOGGER
+    # LOGGER
     logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     logger = logging.getLogger(__name__)
     logger.debug("This is a debug message")
