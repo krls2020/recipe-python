94f3f6334d2ef89bb53b099ddab916628015e291
krls2020 <mail@karelhemza.cz>
2024-06-12 09:11:16 +0200

    8000

--- app.py
+++ app.py
@@ -63,4 +63,4 @@ if __name__ == '__main__':
 
     # Start the Flask application
     from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+    serve(app, host="0.0.0.0", port=8000)
