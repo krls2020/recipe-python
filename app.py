6d8b82eff23b17a3f73322ed9fa71b16147b9429
krls2020 <mail@karelhemza.cz>
2024-06-12 08:59:42 +0200

    waitress

--- app.py
+++ app.py
@@ -52,4 +52,5 @@ if __name__ == '__main__':
     init_db()
 
     # Start the Flask application
-    app.run(host='0.0.0.0', port=8000)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
